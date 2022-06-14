702d9629927f8ca2b80ae0a2279d582f1c086c67
Conner McGibbon <1stvaulthunter@gmail.com>
2022-06-13 21:59:53 -0400

    Stripped out functions in boid class

--- BoidTests/Boid.h
+++ BoidTests/Boid.h
@@ -23,13 +23,6 @@ private:
 	static std::mt19937 m_rand;
 	static std::uniform_real_distribution<float> randomAngle;
 
-	// have all bois access to a list of their flock
-	static std::vector<Boid>* m_flock;
-
-	// size of the screen containing all boids
-	static int m_width;
-	static int m_height;
-
 	// boid movement variables
 	glm::vec2 m_position;
 	glm::vec2 m_velocity{ 0.0f };
@@ -38,11 +31,6 @@ private:
 	float m_maxSpeed{ 2.0f };
 	// maximum acceleration for turning
 	float m_maxForce{ 0.04f };
-
-	// size of the boid
-	float m_size;
-	float m_senseMult{ 25.0f };
-	float m_seperateMult{ 12.5f };
 
 	Type m_type;
 
@@ -53,34 +41,13 @@ public:
 	Boid();
 	Boid(const glm::vec2& position, Type type, float size);
 
-
-	void seek(const glm::vec2& target);
-	void seek(Type target);
-	void seek();
-
-	void flee(const glm::vec2& target);
-	void flee(Type target);
-	void flee();
-
-	void align(float weight);
-	void seperate(float weight);
-	void cohesion(float weight);
-
-	void pursue(Type target);
-
-	void evade(Type target);
-
 	void update();
 
-	const glm::vec2& getPosition() const { return m_position; }
-	const glm::vec2& getVelocity() const { return m_velocity; }
+	const glm::vec2& getPosition()		const { return m_position; }
+	const glm::vec2& getVelocity()		const { return m_velocity; }
+	const glm::vec2& getAcceleration()	const { return m_acceleration; }
 	float getAngle() const { return glm::atan(m_velocity.y, m_velocity.x); }
-	float getSize() const { return m_size; }
 	Type getType() const { return m_type; }
-
-	// set the screen borders for all boids starting at bottom left corner (0,0) to top right (width,height)
-	static void setBorder(int width, int height);
-	static void setFlock(std::vector<Boid>* flock);
 };
 
 #endif
